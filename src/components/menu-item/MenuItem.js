df13cc6b1a86b3a5a522eb77ef30d9066db98d2d
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-09-26 15:19:05 +0100

    add more style to the menu-item component

--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -4,10 +4,11 @@ import "./MenuItem.css";
 
 function MenuItem({ title, imageUrl, size }) {
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className='content'>
         <h1 className='title'>{title}</h1>
         <span className='subtitle'>SHOP NOW</span>
