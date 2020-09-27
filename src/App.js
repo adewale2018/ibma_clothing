d4a71346341c0ffc9bcf550250489d69254d13df
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-09-27 11:47:43 +0100

    add the collection-preview component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { hot } from "react-hot-loader";
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./pages/homepage/Home";
+import ShopPage from "./pages/shop/Shop";
 import "./App.css";
+
+function otherPage() {
+  return <h1>I am the other page!!!</h1>;
+}
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
+      <Route exact path='/shop' component={ShopPage} />
     </Switch>
   );
 }
