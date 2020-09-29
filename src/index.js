d22898391f72aca0ddcfb85831e7563bb60636e6
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-09-29 09:09:44 +0100

    set up redux to manage the state in the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
