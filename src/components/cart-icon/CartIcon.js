77ac592aa016e628fc22196aaf5f8e2fbc709614
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-09-30 17:11:01 +0100

    implement add to cart functinality with redux

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/11.2 shopping-bag.svg";
-import toggleCartHidden from "../../redux/cart/cart-actions";
+import { toggleCartHidden } from "../../redux/cart/cart-actions";
 import "./CartIcon.css";
 
 function CartIcon({ toggleCartHidden }) {
--- src/components/item-collection/ItemCollection.js
+++ src/components/item-collection/ItemCollection.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import "./ItemCollection.css";
 import CustomButton from "../custom-button/CustomButton";
+import { addItem } from "../../redux/cart/cart-actions";
 
-function ItemCollection({ id, name, imageUrl, price }) {
+function ItemCollection({ item, addItem }) {
+  const {  name, imageUrl, price } = item;
   return (
     <div className='collection-item'>
       <div
@@ -17,9 +20,15 @@ function ItemCollection({ id, name, imageUrl, price }) {
         <span className='name'>{name}</span>
         <span className='price'>${price}</span>
       </div>
-      <CustomButton inverted>Add To Cart</CustomButton>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add To Cart
+      </CustomButton>
     </div>
   );
 }
 
-export default ItemCollection;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(ItemCollection);
