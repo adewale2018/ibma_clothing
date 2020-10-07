dff9f286c0813f063a6db2243cdeb06c3b1eb314
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-10-07 23:01:37 +0100

    add the shop data to the redux

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import "./Shop.css";
 import CollectionOverview from "../../components/collection-overview/CollectionOverview";
 import Collection from "../collection/Collection";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+import { updateCollections } from "../../redux/shop/shop.actions";
 
-const ShopPage = ({ match }) => {
-  return (
-    <div className='shop-page'>
-      <Route exact path={`${match.path}`} component={CollectionOverview} />
-      <Route path={`${match.path}/:collectionId`} component={Collection} />
-    </div>
-  );
-};
+class ShopPage extends Component {
+  unSubscribeFromSnapshot = null;
 
-export default ShopPage;
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection("collections");
+    this.unSubscribeFromSnapshot = collectionRef.onSnapshot(
+      async (snapShot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
+        updateCollections(collectionsMap);
+      }
+    );
+  }
+  render() {
+    const { match } = this.props;
+    return (
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={Collection} />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,10 +1,16 @@
 import { shopData } from "./shopdata";
+import ShopActionTypes from "./shop.types";
 const INITIAL_STATE = {
   shopData: shopData,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ShopActionTypes.UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload,
+      };
     default:
       return state;
   }
